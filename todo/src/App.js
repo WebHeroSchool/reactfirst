066aa97047372dd49fd3384dcdff15f09e12bfd4
Даниил Фефелов <feden2069@gmail.com>
2020-05-11 16:02:43 +0500

    todo

---
+++ todo/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const isLogo = true;
+const numberOfProject = 1;
+const desireToLearn = true;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1 style={
+          { color: '#61dafb',
+            margin: 0
+          }
+        }>
+          Hi, It's my the {numberOfProject} project!
+        </h1>
+        <p>
+          I did {numberOfProject - 1} react project before 
+        </p>
+        <p>
+          {isLogo && 'It has a mesmerizing logo'}
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>
+          undefined: {undefined},
+          null: {null},
+          false: {false},
+          true: {true}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
