c18f5dbff1439da0f9288d9d120ad9b0f2999f69
Даниил Фефелов <feden2069@gmail.com>
2020-05-12 14:26:32 +0500

    exp-imp-fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as nums from './numbers'
+import * as nums from './numbers.js';
 
 const isLogo = true;
 const numberOfProject = 1;
@@ -11,16 +11,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1 style={
-          { color: '#61dafb',
-            margin: 0
-          }
-        }>
-          Hi, It's my the {numberOfProject} project!
-        </h1>
-        <p>
-          I did {numberOfProject - 1} react project before 
-        </p>
         <p>
           {isLogo && 'It has a mesmerizing logo'}
         </p>
