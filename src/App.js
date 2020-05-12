e20bdcdaae3c54f25a2f3376381a1bf28f8676b7
Даниил Фефелов <feden2069@gmail.com>
2020-05-12 15:27:23 +0500

    white

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import * as nums from './numbers.js';
+import React from "react";
 
-const isLogo = true;
-const numberOfProject = 1;
-const desireToLearn = true;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {isLogo && 'It has a mesmerizing logo'}
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {desireToLearn ? 'Learn React' : "Don't use the link"}
-        </a>
-        <p>
-         count * length = {nums.count*nums.length}
-       </p>
-      </header>
-    </div>
+const element = (<div>
+  Hello!
+  </div>
   );
-}
 
-export default App;
--- src/numbers.js
+++
@@ -1,2 +0,0 @@
-export const count = 74;
-export const lenght = 9;
